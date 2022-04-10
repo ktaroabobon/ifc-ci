893d22896256bf852f8ccb7ec884966e22bcc9f1
ktaroabobon <ktaroabobon@gmail.com>
2022-04-10 15:01:25 +0900

    update: standard_method.py

---
+++ ifc_ci/law/base_method.py
@@ -0,0 +1,6 @@
+from typing import Optional
+
+
+class StandardMethod(object):
+    def __init__(self, ifc_file):
+        self.ifc_file = ifc_file
---
+++ ifc_ci/law/sub_method.py
@@ -0,0 +1,11 @@
+"""
+建築基準法施行令に関するコード
+"""
+
+from .base_method import StandardMethod
+
+
+class law109_4(StandardMethod):
+    """
+    施行令第109条第4項
+    """
